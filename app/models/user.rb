79b890d588d1b5898639e823c10b4fd742d85a41
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-11 22:19:09 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ActiveRecord::Base
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :reviews, :foreign_key => 'guest_id'
 
-  has_many :trip_listings, through: :listings, source: :listing
-  has_many :hosts, through: :trip_listings, foreign_key: 'host_id'
-
+  has_many :trip_listings, :through => :trips, :source => :listing
+ has_many :hosts, :through => :trip_listings, :foreign_key => :host_id
 
   has_many :guests, through: :reservations, foreign_key: 'guest_id'
 
