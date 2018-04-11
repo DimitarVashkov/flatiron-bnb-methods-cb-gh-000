64359f2adce0e2a3ababd63c6b1d1ca95b67ec7b
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-11 22:15:41 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :reviews, :foreign_key => 'guest_id'
 
+  has_many :trip_listings
   has_many :hosts, class_name: 'User'
   has_many :guests, class_name: 'User'
 
