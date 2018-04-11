4d6102da7f497c489c346c90166269934bc9f7bf
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-11 22:12:22 +0000

    automatically backed up by learn

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,28 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  validates :address, presence: true
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :listing_type, presence: true
+  validates :neighborhood, presence: true
+
+  after_save :set_host
+  before_destroy :unset_host
+
+  def average_review_rating
+    reviews.average(:rating)
+  end
+
+  def set_host
+    host.update(host: true)
+  end
+
+  def unset_host
+    if Listing.where(host: host).where.not(id: id).empty?
+     host.update(host: false)
+   end
+  end
 end
